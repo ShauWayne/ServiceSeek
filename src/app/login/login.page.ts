e23cafc3b64847e7cf5b2eefb9cff8d73e60a6d7
LaCicuta <mati.moralesi@duocuc.cl>
2024-09-09 20:21:55 -0300

    comprobando

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,15 +3,20 @@ import { ModalController } from '@ionic/angular'; // Importa ModalController
 import { Router } from '@angular/router';
 import { RegisterModalComponent } from '../register-modal/register-modal.component'; // Ajusta la ruta si es necesario
 import { RecuperarPasswordComponent } from '../recuperar-password/recuperar-password.component';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor(private modalCtrl: ModalController, private router:Router) { }
+  username: string = ''; // Inicializar como cadena vacía
+  password: string = ''; // Inicializar como cadena vacía
+  errorMessage: string = ''; // Inicializar como cadena vacía
+
+  constructor(private modalCtrl: ModalController, private router:Router, private alertController: AlertController) { }
 
 
   ngOnInit() {
@@ -31,7 +36,43 @@ export class LoginPage implements OnInit {
     return await modal.present();
   }
 
-  irAHome(){
-    this.router.navigate(['/home']);
+  onLogin(){
+    this.errorMessage = '';
+
+    if (!this.isPasswordValid(this.password)) {
+      this.errorMessage = 'La contraseña debe tener al menos 4 números, 3 caracteres especiales, y 1 letra mayúscula.';
+      return;
+    }
+
+    this.showSuccessAlert();
+    
   }
+
+  isPasswordValid(password: string): boolean {
+    // Al menos 4 números
+    const hasNumber = (password.match(/[0-9]/g) || []).length >= 4;
+    // Al menos 1 letra mayúscula
+    const hasUpperCase = /[A-Z]/.test(password);
+    // Al menos 3 caracteres especiales
+    const hasCharacter = (password.match(/[!@#$%^&*(),.?":{}|<>_-]/g) || []).length >= 3;
+
+    return hasNumber && hasUpperCase && hasCharacter && password.length >= 8;
+  }
+
+  async showSuccessAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Bienvenido a AhorraSmart!',
+      message: ' Toma las riendas de tus finanzas.',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          this.router.navigate(['/home']);
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+  
+  
 }
