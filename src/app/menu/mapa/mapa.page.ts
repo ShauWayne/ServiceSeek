6b435151c7fe554948ef05c19b29006ae430e907
ShauWayne <machine.queen@gmail.com>
2024-10-14 11:00:50 -0300

    Se agrega API Mapa

--- src/app/menu/mapa/mapa.page.ts
+++ src/app/menu/mapa/mapa.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.page.scss'],
 })
 export class MapaPage implements OnInit {
+  map = null;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadMap();
   }
 
-}
+  loadMap() {
+    const mapEle: HTMLElement | null = document.getElementById('map');
+    if (mapEle) { // Verifica si no es null
+      const myLatLng = { lat: -33.45694, lng: -70.64827 };
+      this.map = new google.maps.Map(mapEle, { 
+        center: myLatLng,
+        zoom: 12,
+      });
+      google.maps.event.addListenerOnce(this.map, 'idle', () => {
+        mapEle.classList.add('show-map');
+      });
+    } else {
+      console.error('El elemento con ID "map" no se encontró en el DOM.');
+    }
+  }
+}  
+
